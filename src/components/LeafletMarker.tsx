174ec9511f26774dbe977ae85e92f8070209b4cb
A <aaafff93@yandex.ru>
2025-09-28 11:01:16 +0300

    Add theme

--- src/components/LeafletMarker.tsx
+++ src/components/LeafletMarker.tsx
@@ -1,10 +1,29 @@
 import {Marker} from "react-leaflet";
-import type {IMarkerProps} from "../shared/types.ts";
+import type {IMarkerProps} from "../shared/types/types.ts";
+import {divIcon} from "leaflet";
+import SvgIcon from "./SvgIcon.tsx";
+import {renderToString} from "react-dom/server";
 
-const LeafletMarker = ({position}: IMarkerProps) => {
+/*const customIcon = new Icon({
+    iconUrl: marker,
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+})*/
+
+const LeafletMarker = ({position, color = 'success'}: IMarkerProps) => {
+
+    const icon = divIcon({
+        html: renderToString(
+            <SvgIcon fill={color}/>
+        ),
+        className: '',
+        iconAnchor: [15, 30]
+    });
+
     return (
         <Marker
             position={position}
+            icon={icon}
         />
     );
 };
--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,19 +1,27 @@
 import type {LatLngExpression} from "leaflet";
 import type {ReactNode} from "react";
+import type {FillColors} from "./theme.ts";
 
 export type LatLng = [number, number];
 
 export interface IMarkerDTO {
     id: number;
     position: LatLng;
+    isVisited: boolean;
 }
 
 export interface IMarkerProps {
     position: LatLng;
+    color?: FillColors;
 }
 
 export interface ILeafletMapProps {
     center?: LatLngExpression;
     zoom: number;
     children: ReactNode[];
+}
+
+
+export interface SvgIconProps {
+    fill?: FillColors;
 }
